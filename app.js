11b0eed4f5f363e4e767f07901b71c1fac1ec90d
Jared Easterday <jared.easterday@gmail.com>
2015-02-02 20:51:58 -0800

    cleaned up how mines are randomed, tile view

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 (function(){
 
   var TileView = Backbone.View.extend({
+    className: 'tile',
+    template: _.template("<%- hasMine %>"),
+
     render: function(){
-      this.$el.html('tile: ' + this.model.get('hasMine'));
+      this.$el.html(this.template(this.model.toJSON()));
       return this;
     }
   });
 
   var Game = Backbone.View.extend({
     initialize: function(){
-      _(this).bindAll('renderTiles', 'renderTile', 'render');
+      _(this).bindAll('renderTiles', 'renderTile', 'render', 'buildTiles');
       this.tiles = new TilesCollection();
 
       this.mines = 10;
 
-      for (var i = 0; i < 100; i++){
-        var hasMine = this.mines && _.random(1);
-
-        if (hasMine){
-          this.mines -= 1;
-        }
-
-        var model = new TileModel({
-          hasMine: hasMine
-        });
-
-        this.tiles.add(model);
-      }
+      _(100).times(this.buildTiles);
 
       this.renderTiles();
     },
 
+    buildTiles: function(i){
+      var model = new TileModel({
+        hasMine: this.mines < 1 ? false : true
+      });
+
+      this.tiles.add(model);
+
+      this.mines -= 1;
+    },
+
     renderTiles: function(){
+      this.tiles.reset(this.tiles.shuffle());
       this.tiles.each(this.renderTile);
     },
 
@@ -43,6 +45,8 @@
     },
 
     render: function(){
+      // renderForm
+      // renderTiles
       return this;
     }
   });
