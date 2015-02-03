d77072de008845a079e37a3d0a7a849946e1af2c
Jared Easterday <jared.easterday@gmail.com>
2015-02-03 00:06:58 -0800

    Basic flagging and expose behavior

--- app.js
+++ app.js
@@ -2,15 +2,41 @@
 
   var TileView = Backbone.View.extend({
     className: 'tile',
-    template: _.template("<%- hasMine %>"),
+    template: _.template("<div class='<%= state %>'><%= hasMine %></div>"),
+    events: {
+      'mousedown' : 'onTileClick'
+    },
+
+    initialize: function(){
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    onTileClick: function(event){
+      console.log(this.model.get('hasMine'));
+      switch (event.which) {
+        case 1:
+          console.log('Left Mouse button pressed.');
+          this.model.set({ state: 'exposed'});
+          break;
+        case 3:
+          console.log('Right Mouse button pressed.');
+          this.model.set({ state: 'flagged'});
+          break;
+        default: return;
+      }
+
+    },
 
     render: function(){
+      console.log('tile render', this.model.get('state'));
       this.$el.html(this.template(this.model.toJSON()));
       return this;
     }
   });
 
   var Game = Backbone.View.extend({
+    className: 'board',
+
     initialize: function(){
       _(this).bindAll('renderTiles', 'renderTile', 'render', 'buildTiles');
       this.tiles = new TilesCollection();
@@ -52,6 +78,10 @@
   });
 
   var TileModel = Backbone.Model.extend({
+    defaults: {
+      'state' : 'blank'
+    },
+
     initialize: function(options){
       this.hasMine = options.hasMine;
     }
